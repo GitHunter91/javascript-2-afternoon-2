9fb6187e3ed530c2c1cf95f110d969ad1146dce2
Hunter Sexton <h.sexton@live.com>
2018-03-13 17:07:22 -0700

    next steps

--- practice.js
+++ practice.js
@@ -227,7 +227,13 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
   *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 */
   
-//Code Here
+const addTen = (numbers)=>{
+  var plusTen = [];
+  for(i=0;i<numbers.length;i++){
+    plusTen.push(numbers[i]+10);
+ }
+ return plusTen;
+}
 
 
 
